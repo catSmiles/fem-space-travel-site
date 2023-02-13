2d014c0bcc4a95d65051dcab23747caa5e53cdc0
levantrung81 <levantrung.fe2101@gmail.com>
2023-02-13 22:19:16 +0700

    The tabs - Keyboard navigation part 3

--- tabs.js
+++ tabs.js
@@ -1,9 +1,13 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-let tabFocus = 0;
 
-tabList.addEventListener('keydown', (e) => {
+tabList.addEventListener('keydown', changeTabFocus);
+
+
+
+let tabFocus = 0;
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
 
@@ -13,11 +17,18 @@ tabList.addEventListener('keydown', (e) => {
 
     if (e.keyCode === keydownRight) {
         tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
     }
 
     if (e.keyCode === keydownLeft) {
         tabFocus--;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }
     }
 
-
-})
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+}
